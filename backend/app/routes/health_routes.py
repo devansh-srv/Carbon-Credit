562b5038caffd277168dfc7ec34dd71eb64cf29b
Devansh Srivastava <102247720+devansh-srv@users.noreply.github.com>
2025-01-21 15:57:55 +0530

    Revert "health route"

--- backend/app/routes/health_routes.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, request, jsonify
-
-health_bp = Blueprint('health', __name__)
-
-@health_bp.route('/health-check', methods=["GET"])
-def heath_check():
-    return jsonify({'status':'Up'}), 200
